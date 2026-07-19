4834f506e58f5973bdcede77256b0d4551192325
yienard0 <17159130+yienard0@user.noreply.gitee.com>
2026-07-20 03:18:49 +0800

    Auto-save

--- career-os/scrapers/print_results.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import json
-r = json.load(open('/tmp/greenhouse_results.json'))
-print(f"Total new jobs: {r['new_count']}")
-for j in r['new_jobs']:
-    print(f"  {j['title']} | {j['company']} | {j['location']} | {j['url']}")
-print(f"\nErrors: {r['errors']}")
